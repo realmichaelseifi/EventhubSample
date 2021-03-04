419cc670468c653ee99100d72471fabd2687b6a7
Michael Seifi <michael.seifi@fairwaymc.com>
2021-03-04 11:04:40 -0600

    Add project files.

---
+++ EventhubSampleReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventhubSampleReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EventhubSampleWriter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+
+namespace EventhubSampleWriter
+{
+    class Program
+    {
+        private const string connectionString = "";
+        private const string eventHub = "";
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await using (var producerCLient = new EventHubProducerClient(connectionString: connectionString, eventHubName: eventHub))
+            {
+                // create a batch of events
+                using EventDataBatch eventDataBatch = await producerCLient.CreateBatchAsync();
+
+
+            }
+        }
+    }
+}
